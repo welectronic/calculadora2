9451485cde86a1f8c47e005c33015ed1b440f4af
wrojas <welectronic@gmail.com>
2024-07-23 19:42:21 -0500

    Final cambios Javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const calculatorInstance = new Calculator(new Display());
+
+    const buttonElements = document.querySelectorAll('.button');
+    buttonElements.forEach(buttonElement => {
+        const value = buttonElement.getAttribute('data-value');
+        new Button(buttonElement,() => calculatorInstance.pressButton(value) );
+    });
+    
 }
 
 )
